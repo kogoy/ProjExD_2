e9381ebc49e73fd39af71e206ac4449402eef8ea
kogo <c0b230634c@edu.teu.ac.jp>
2024-11-26 14:51:25 +0900

    れんしゅう4:しょうとつはんてい

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -49,6 +49,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")
+            return  # ゲームオーバー
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
